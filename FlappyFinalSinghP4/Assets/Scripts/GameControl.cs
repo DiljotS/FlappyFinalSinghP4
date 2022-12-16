e17f3e2a999b902cbdc87a059f25b8eb2dd126b2
Singh <730222@student.dusd.net>
2022-12-16 11:42:27 -0800

    Started Final
    Finished everything up to game controller.

---
+++ FlappyFinalSinghP4/Assets/Scripts/GameControl.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+    public GameObject gameOvertext;
+    public bool gameOver = false;
+
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if (gameOver && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void BirdDied()
+    {
+        gameOvertext.SetActive(true);
+        gameOver = true;
+    }
+}
